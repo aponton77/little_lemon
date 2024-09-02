0faa7fad9e1de3014f5d506628bb3622742999d1
Alan <alanponton.ap@gmail.com>
2024-09-02 05:55:02 -0500

    commit 2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react'
 import logo from '../images/Logo .svg'
 
@@ -10,13 +11,35 @@ const Nav = () => {
         {/* mobile navbar */}
         <div>
             <div className='bar'></div>
+=======
+import React, { useState } from 'react'
+import logo from '../images/Logo .svg'
+
+const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+  return (
+    <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+        <a href='/' className='logo'>
+        <img src={logo} alt='logo'/>
+        </a>
+        {/* mobile navbar */}
+        <div className="menu-icon" onClick={toggleMenu}>
+>>>>>>> 6b0ea67 (commit 2)
             <div className='bar'></div>
             <div className='bar'></div>
             <div className='bar'></div>
         </div>
 
         {/* nav items */}
+<<<<<<< HEAD
         <ul>
+=======
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+>>>>>>> 6b0ea67 (commit 2)
             <li>
                 <a href='/'>Home</a>
             </li>
